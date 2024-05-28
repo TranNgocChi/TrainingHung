f16f8567297ad9fad5e0410baaffa4161d3a5ec6
TranNgocChi <brightsuntnc2003@gmail.com>
2024-05-28 22:19:31 +0700

    Studying

---
+++ BusinessLayer/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [StringLength(100, ErrorMessage="Ko duoc qua 100")]
+        [Required]
+        public string? Name { get; set; }
+
+        [StringLength(100, ErrorMessage = "Ko duoc qua 100")]
+        [Required]
+        public string? Description { get; set; }
+
+    }
+}
---
+++ PresentationLayer/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Models;
+using DataLayer.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace PresentationLayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(ILogger<HomeController> logger, IProductRepository productRepository)
+        {
+            _logger = logger;
+            _productRepository = productRepository; 
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _productRepository.GetAll();   
+            return View(products);
+        }
+
+        public IActionResult Create(string name, string description)
+        {
+            _productRepository.Create(new Product { Name = name, Description = description });
+            return View("Views/Home/Index.cshtml", _productRepository.GetAll());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
